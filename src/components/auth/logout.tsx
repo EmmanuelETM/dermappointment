5dddd639ba043e17bb5aaad1e1258dc6ff50f2b5
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-03-03 11:42:39 -0400

    Logout is working now

---
+++ src/components/auth/logout.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { logout } from "@/actions/logout";
+import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { LogOut } from "lucide-react";
+
+export const LogoutItem = () => {
+  return (
+    <DropdownMenuItem onClick={async () => await logout()}>
+      <div className="flex">
+        <LogOut /> Log out
+      </div>
+    </DropdownMenuItem>
+  );
+};
