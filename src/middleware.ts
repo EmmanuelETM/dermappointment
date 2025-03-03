f8d2a9ebb2ecf37c74f36cdcd0696ab2bbf23fcd
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-03-02 23:03:02 -0400

    final test

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,10 @@ import {
 } from "@/routes";
 import { NextResponse } from "next/server";
 
-export default auth((req) => {
+export default auth(async (req) => {
   const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
+  const session = await auth();
+  const isLoggedIn = !!session;
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
@@ -17,15 +18,15 @@ export default auth((req) => {
     return NextResponse.next();
   }
   if (isAuthRoute) {
-    if (!isLoggedIn) {
-      return Response.redirect(new URL("/login", nextUrl));
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
     return NextResponse.next();
   }
-  // if (!isLoggedIn && !isPublicRoute) {
-  //   return Response.redirect(new URL("/login", nextUrl));
-  // }
-  // return NextResponse.next();
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/login", nextUrl));
+  }
+  return NextResponse.next();
 });
 
 export const config = {
