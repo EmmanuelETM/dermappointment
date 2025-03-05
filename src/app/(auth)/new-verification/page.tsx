ec79da5bd18eb729327286bcd83dac1c546b7053
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-03-04 20:08:08 -0400

    Add Suspense tag so we can use useSearchParams()

--- src/app/(auth)/new-verification/page.tsx
+++ src/app/(auth)/new-verification/page.tsx
@@ -1,7 +1,12 @@
 import { NewVerificationForm } from "@/components/auth/new-verification/new-verification-form";
+import { Suspense } from "react";
 
 function AuthErrorPage() {
-  return <NewVerificationForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NewVerificationForm />
+    </Suspense>
+  );
 }
 
 export default AuthErrorPage;
