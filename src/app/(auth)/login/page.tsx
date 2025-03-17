57d6a506c470ece0fd1079c06c5a5a9d7545edae
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-03-16 23:58:18 -0400

    fixing more stuff

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,11 @@
 import { LoginForm } from "@/components/auth/login/login-form";
+import { Suspense } from "react";
 
 function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <LoginForm />
+    </Suspense>
+  );
 }
-
 export default LoginPage;
