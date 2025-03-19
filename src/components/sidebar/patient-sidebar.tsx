b779b096d30d03b600acd312d0657ccaf0d456c2
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-03-19 00:03:55 -0400

    scaffold basic sidebar

--- src/components/sidebar/patient-sidebar.tsx
+++ src/components/sidebar/patient-sidebar.tsx
@@ -16,79 +16,57 @@ import {
 } from "@/components/ui/sidebar";
 import { NavUser } from "@/components/nav-user";
 
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
 const data = {
   user: {
     name: "shadcn",
     email: "m@example.com",
     image: "https://robohash.org/1",
   },
-  navMain: [
-    {
-      title: "Home",
-      url: "#",
-      items: [
-        {
-          title: "Appointments",
-          url: "/patient/dashboard",
-        },
-        {
-          title: "Doctors",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-      ],
-    },
-  ],
 };
 
-export function PatientSidebar({
-  ...props
-}: React.ComponentProps<typeof Sidebar>) {
+const items = [
+  {
+    title: "Home",
+    url: "/patient/home",
+    icon: Home,
+  },
+  {
+    title: "Appointments",
+    url: "/patient/appointments",
+    icon: Calendar,
+  },
+];
+
+export function PatientSidebar() {
   const user = useCurrentUser();
   data.user.name = user?.name ?? "";
   data.user.email = user?.email ?? "";
   data.user.image = user?.image ?? "";
-
   return (
-    <Sidebar {...props}>
+    <Sidebar>
       <SidebarHeader>
         <NavUser user={data.user} />
       </SidebarHeader>
       <SidebarContent>
-        {data.navMain.map((item) => (
-          <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {item.items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        ))}
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
