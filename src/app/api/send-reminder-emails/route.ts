839fa9393d1b3f4568e0f83648105e5cfead4dbc
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2025-04-14 14:13:05 -0400

    Testing notification logs

--- src/app/api/send-reminder-emails/route.ts
+++ src/app/api/send-reminder-emails/route.ts
@@ -3,8 +3,8 @@ import { env } from "@/env";
 import { addDays, startOfDay, endOfDay } from "date-fns";
 import { getAppointmentsInRange } from "@/data/appointments";
 import { sendReminderEmail } from "@/lib/mail/appointment";
-import { db } from "@/server/db"; // Suponiendo que tienes acceso a la base de datos aquí
-import { notificationLogs } from "@/server/db/schema"; // Asegúrate de tener la tabla correcta
+import { db } from "@/server/db";
+import { notificationLogs } from "@/server/db/schema";
 import { and, eq, gte } from "drizzle-orm";
 
 export async function POST(req: Request) {
@@ -15,11 +15,10 @@ export async function POST(req: Request) {
   }
 
   const now = new Date();
-  const startOfTomorrow = startOfDay(addDays(now, 1)); // Empieza a las 00:00 de mañana
-  const endOfTomorrow = endOfDay(startOfTomorrow); // Fin de mañana a las 23:59
+  const startOfTomorrow = startOfDay(addDays(now, 1));
+  const endOfTomorrow = endOfDay(startOfTomorrow);
 
   try {
-    // Obtener citas para el día siguiente (mañana)
     const appointmentsForTomorrow = await getAppointmentsInRange(
       startOfTomorrow,
       endOfTomorrow,
@@ -34,34 +33,30 @@ export async function POST(req: Request) {
 
     const sendResults = await Promise.allSettled(
       appointmentsForTomorrow.map(async (appt) => {
-        // Verificar si ya existe un registro de notificación para este usuario y cita
-
         const existingLog = await db.query.notificationLogs.findFirst({
           where: and(
             eq(notificationLogs.userId, appt.patients.id),
             eq(notificationLogs.type, "Email"),
             eq(notificationLogs.status, "Sent"),
-            gte(notificationLogs.createdAt, startOfTomorrow), // Compara con el inicio de mañana
+            gte(notificationLogs.createdAt, startOfDay(new Date())),
           ),
         });
 
         if (!existingLog) {
-          // Si no existe un log, entonces enviamos el correo
           await sendReminderEmail(appt.patients.email!, appt);
 
-          // Insertar log en la base de datos
           await db.insert(notificationLogs).values({
             userId: appt.patients.id,
             type: "Email",
             content: `Reminder email sent for appointment ${appt.id}`,
             status: "Sent",
-            createdAt: new Date(), // Si es necesario para tener registro de cuándo se envió
+            createdAt: new Date(),
           });
 
           return { status: "fulfilled" };
         }
 
-        return { status: "rejected" }; // Si el log ya existe, no enviamos el correo
+        return { status: "rejected" };
       }),
     );
 
@@ -81,7 +76,7 @@ export async function POST(req: Request) {
       { status: 200 },
     );
   } catch (error) {
-    console.error("Error en el cron job:", error); // Mejor para depuración
+    console.error("Error en el cron job:", error);
     return NextResponse.json(
       { error: "Ocurrió un error al procesar los correos" },
       { status: 500 },
